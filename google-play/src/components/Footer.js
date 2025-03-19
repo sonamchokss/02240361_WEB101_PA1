d540e5fa818a9742dcd4cc7ef0910eeb47985466
sonamchokss <chokipromax@gmail.com>
2025-03-19 12:29:00 +0530

    Delete google-play directory

--- google-play/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="p-4 text-center text-sm text-gray-500 bg-white">
-      &copy; 2025 Google Play | Privacy | Terms | Help
-    </footer>
-  );
-}
